21b0904b40d6facbf598e5976c48c486b8836251
Jesus Sanchez <113047026+JesusSanchezM@users.noreply.github.com>
2024-01-27 22:16:02 -0600

    .

--- Macroeconomia.R
+++ Macroeconomia.R
@@ -1,3 +1,38 @@
 #-----------------------------
 #------Macroeconomía---------
 #-----------------------------
+
+library(tidyverse)
+
+#Actividad 1 
+{
+#Cambios en la demanda y la oferta
+
+Precio <- c(10, 20, 30, 40, 50, 60)
+Cantidad <- c(100, 90, 80, 70, 60, 50)
+Demanda <- data.frame(Precio, Cantidad)
+
+ggplot() + geom_line(aes(x=Cantidad, y=Precio), color="black") + theme_classic()
+
+formattable(Demanda, list(
+  Cantidad = color_tile("white", "orange")),
+  Precio = color_tile("white", "red"))
+
+ 
+datatable(Demanda)
+
+kable(Demanda) %>% 
+  kable_styling("striped", full_width = T) %>% 
+  row_spec(0, bold=T, color="black", background = "turquoise") %>% 
+  row_spec(1:6, bold=T, color="black", background = "pink") 
+  
+
+}
+
+
+
+
+
+
+
+
