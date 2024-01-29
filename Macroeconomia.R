c334a0c94e6045519dc3dd0c35cd530b22d529ba
Jesus Sanchez <113047026+JesusSanchezM@users.noreply.github.com>
2024-01-28 18:26:21 -0600

    .

--- Macroeconomia.R
+++ Macroeconomia.R
@@ -48,5 +48,55 @@ ggplot(Demanda_1, aes(x = Cantidad_original, y = Precio, group = `Ocurre un...`)
   scale_color_manual(values = c("blue", "orange", "green", "purple", "red")) +
   scale_linetype_manual(values = c("solid", "solid", "solid", "dashed", "dashed"))
 
+kable(Demanda) %>% 
+  kable_styling("striped", full_width = F) %>% 
+  row_spec(1:6, bold=F, background = "turquoise") %>% 
+  column_spec(1, bold=T, background = "white") 
+  
+
 }
+
+# Gráficas incisos e) al h)
+{
+  
+Incremento_precio_bien_sustituto <- Cantidad_original+10
+Incremento_precio_bien_complementario <- Cantidad_original-10
+Decremento_precio_bien_sustituto <- Incremento_precio_bien_complementario
+Decremento_precio_bien_complementario <- Incremento_precio_bien_sustituto
+
+Demanda <- data.frame(Precio, Cantidad_original, 
+                      Incremento_precio_bien_sustituto,
+                      Incremento_precio_bien_complementario, 
+                      Decremento_precio_bien_sustituto,
+                      Decremento_precio_bien_complementario)
+
+Demanda_2 <- Demanda %>% gather(key="Ocurre un...",value = "Cantidad_original",-Precio)
+
+
+ggplot(Demanda_2, aes(x = Cantidad_original, y = Precio, group = `Ocurre un...`)) +
+  geom_line(aes(color = `Ocurre un...`, linetype = `Ocurre un...`), size = 1.5) +
+  theme(axis.text.x = element_text(angle = 90)) +
+  theme_classic() +
+  labs(title = "Movimientos en la demanda",
+       subtitle = "Variación por unidad",
+       x = "Cantidad", y = "Precio",
+       caption = "Fuente: Elaboración propia con datos propios") +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold", size = 18),
+        plot.caption = element_text(hjust = 0, face = "bold", size = 10),
+        plot.subtitle = element_text(hjust = 0.5, face = "bold", size = 12),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+  scale_color_manual(values = c("grey59", "darkorchid", "green", "cornflowerblue", "#FF69B4")) +
+  scale_linetype_manual(values = c("solid", "solid", "solid", "dashed", "dashed"))
+
+
+kable(Demanda) %>% 
+  kable_styling("striped", full_width = F) %>% 
+  row_spec(1:6, bold=F, background = "coral") %>% 
+  column_spec(1, bold=T, background = "white")  
+
+  
+}
+
+
+
 
